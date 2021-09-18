383e8308678a54de03821ddb3cd4b8c75d00dcef
Matthew Rosell Felix <matthewgz241@gmail.com>
2021-09-18 01:42:14 -0500

    [Feature] - Integrating GitHub api in the project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
+@ApiTags('/')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Say Hello' })
   getHello(): string {
     return this.appService.getHello();
   }
