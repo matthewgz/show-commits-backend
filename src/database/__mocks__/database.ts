1de597f975a21ca1a6d628953e69d003215f46ac
Matthew Rosell Felix <matthewgz241@gmail.com>
2021-09-19 18:06:11 -0500

    [Update] - Updating the repositories' response

--- src/database/__mocks__/database.ts
+++ src/database/__mocks__/database.ts
@@ -2,8 +2,8 @@ import { DATABASE } from 'database/entities/database.entity';
 
 export const database: DATABASE = {
   repositories: [
-    { name: 'show-commits-backend' },
-    { name: 'show-commits-frontend' },
+    { name: 'show-commits-backend', label: 'Backend' },
+    { name: 'show-commits-frontend', label: 'Frontend' },
   ],
   user: 'matthewgz',
 };
--- src/repositories/entities/repositories.entity.ts
+++ src/repositories/entities/repositories.entity.ts
@@ -1,4 +1,5 @@
 export class Repository {
+  label: string;
   name: string;
 }
 
